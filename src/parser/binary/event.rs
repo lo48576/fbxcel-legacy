c267fe3ab7bccbec12ec8288eb7e42c8ad15fac8
Takuma YOSHIOKA <tashioka.256@gmail.com>
2016-10-31 14:31:13 +0900

    Implement node header struct and its reader function

--- src/parser/binary/event.rs
+++ src/parser/binary/event.rs
@@ -1,5 +1,6 @@
 //! Parser event.
 
+use std::io;
 use std::io::Read;
 
 use super::BinaryParser;
@@ -146,5 +147,51 @@ impl StartNodeBuilder {
     /// Creates `StartNode` from the `StartNodeBuilder` and the given parser.
     pub fn build<R: Read>(self, parser: &mut BinaryParser<R>) -> StartNode<R> {
         StartNode { _parser: parser }
+    }
+}
+
+
+/// Fixed size node header (without node name field).
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct NodeHeader {
+    /// End offset of the node.
+    pub end_offset: u64,
+    /// Number of the node attributes.
+    pub num_attributes: u64,
+    /// Byte length of the node attributes.
+    pub len_attributes: u64,
+    /// Length of the node name.
+    pub len_name: u8,
+}
+
+impl NodeHeader {
+    /// Returns true if all fields of the node header is `0`.
+    pub fn is_node_end(&self) -> bool {
+        self.end_offset == 0 && self.num_attributes == 0 && self.len_attributes == 0 &&
+        self.len_name == 0
+    }
+
+    /// Reads node header from the given parser and returns it.
+    pub fn read_from_parser<R: Read>(parser: &mut BinaryParser<R>) -> io::Result<Self> {
+        let fbx_version = parser.fbx_version
+            .expect("Attempt to read FBX node header but the parser doesn't know FBX version");
+        let (end_offset, num_attributes, len_attributes) = if fbx_version < 7500 {
+            let eo = try!(parser.source.read_u32()) as u64;
+            let na = try!(parser.source.read_u32()) as u64;
+            let la = try!(parser.source.read_u32()) as u64;
+            (eo, na, la)
+        } else {
+            let eo = try!(parser.source.read_u64());
+            let na = try!(parser.source.read_u64());
+            let la = try!(parser.source.read_u64());
+            (eo, na, la)
+        };
+        let len_name = try!(parser.source.read_u8());
+        Ok(NodeHeader {
+            end_offset: end_offset,
+            num_attributes: num_attributes,
+            len_attributes: len_attributes,
+            len_name: len_name,
+        })
     }
 }
