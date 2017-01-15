fdf6c4ffe509a9d1eda79e66c4d5e4ef02707370
Takuma YOSHIOKA <tashioka.256@gmail.com>
2017-01-16 01:42:16 +0900

    Add variants to loader error

--- src/loader/binary/simple/error.rs
+++ src/loader/binary/simple/error.rs
@@ -13,13 +13,27 @@ pub type Result<T> = ::std::result::Result<T, Error>;
 /// Load error.
 #[derive(Debug, Clone)]
 pub enum Error {
+    /// Attribute is invalid.
+    ///
+    /// This includes the cases below:
+    /// - The node has too few or too many node attributes
+    /// - The node has wrong types of node attribute values.
+    /// - The node has wrong (or unsupported) values of node attributes values.
+    InvalidAttribute(String),
+    /// Required node is missing.
+    MissingNode(String),
     /// Parse error (including I/O error).
     Parse(ParseError),
+    /// Got an unexpected node.
+    UnexpectedNode(String),
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
+            Error::InvalidAttribute(ref name) => write!(f, "Invalid attribute for node: {}", name),
+            Error::MissingNode(ref name) => write!(f, "Missing node: {}", name),
+            Error::UnexpectedNode(ref name) => write!(f, "Unexpected node: {}", name),
             _ => write!(f, "{}", (self as &error::Error).description()),
         }
     }
@@ -28,6 +42,9 @@ impl fmt::Display for Error {
 impl error::Error for Error {
     fn description(&self) -> &str {
         match *self {
+            Error::InvalidAttribute(_) => "Invalid node attribute",
+            Error::MissingNode(_) => "Missing node",
+            Error::UnexpectedNode(_) => "Unexpected node",
             Error::Parse(ref err) => err.description(),
         }
     }
@@ -35,6 +52,7 @@ impl error::Error for Error {
     fn cause(&self) -> Option<&error::Error> {
         match *self {
             Error::Parse(ref err) => Some(err),
+            _ => None,
         }
     }
 }
