75335e750846958be2de73421a5d13174760167a
Takuma YOSHIOKA <tashioka.256@gmail.com>
2017-01-21 21:34:02 +0900

    Add root node types
    They are temporary implementations yet and their children are held by
    vector of `GenericNode`.

--- src/loader/binary/simple/fbx7400/mod.rs
+++ src/loader/binary/simple/fbx7400/mod.rs
@@ -1,7 +1,7 @@
 //! Simple FBX 7.4 binary loader.
 
-use parser::binary::{Result, Parser, ParserSource, FbxFooter};
-use loader::binary::simple::GenericNode;
+use parser::binary::{Parser, ParserSource, FbxFooter, Event, Attributes};
+use loader::binary::simple::{Result, Error, GenericNode};
 
 
 /// FBX 7.4 or later.
@@ -9,8 +9,28 @@ use loader::binary::simple::GenericNode;
 pub struct Fbx7400 {
     /// FBX version.
     pub version: u32,
-    /// Nodes.
-    pub nodes: Vec<GenericNode>,
+    /// `FBXHeaderExtension`.
+    pub fbx_header_extension: FbxHeaderExtension,
+    /// `FileId`.
+    pub file_id: FileId,
+    /// `CreationTime`.
+    pub creation_time: CreationTime,
+    /// `Creator`.
+    pub creator: Creator,
+    /// `References`.
+    pub references: References,
+    /// `GlobalSettings`.
+    pub global_settings: GlobalSettings,
+    /// `Documents`.
+    pub documents: Documents,
+    /// `Definitions`.
+    pub definitions: Definitions,
+    /// `Objects`.
+    pub objects: Objects,
+    /// `Connections`.
+    pub connections: Connections,
+    /// `Takes`.
+    pub takes: Option<Takes>,
     /// FBX footer.
     pub footer: Option<FbxFooter>,
 }
@@ -23,11 +43,300 @@ impl Fbx7400 {
     ) -> Result<Self> {
         info!("FBX version: {}, loading in FBX 7400 mode", version);
 
-        let (nodes, footer) = GenericNode::load_from_parser(&mut parser)?;
+        let footer;
+        let mut fbx_header_extension = None;
+        let mut file_id = None;
+        let mut creation_time = None;
+        let mut creator = None;
+        let mut global_settings = None;
+        let mut documents = None;
+        let mut references = None;
+        let mut definitions = None;
+        let mut objects = None;
+        let mut connections = None;
+        let mut takes = None;
+        loop {
+            let node_type = match parser.next_event()? {
+                Event::StartFbx(_) |
+                Event::EndNode => unreachable!(),
+                Event::EndFbx(f) => {
+                    footer = f.ok();
+                    break;
+                },
+                Event::StartNode(info) => NodeType::load(info.name, info.attributes)?,
+            };
+            debug!("node_type: {:?}", node_type);
+            match node_type {
+                NodeType::FbxHeaderExtension => {
+                    fbx_header_extension = Some(FbxHeaderExtension::load(parser.subtree_parser())?);
+                },
+                NodeType::FileId(attrs) => {
+                    file_id = Some(FileId::load(parser.subtree_parser(), attrs)?);
+                },
+                NodeType::CreationTime(attrs) => {
+                    creation_time = Some(CreationTime::load(parser.subtree_parser(), attrs)?);
+                },
+                NodeType::Creator(attrs) => {
+                    creator = Some(Creator::load(parser.subtree_parser(), attrs)?);
+                },
+                NodeType::GlobalSettings => {
+                    global_settings = Some(GlobalSettings::load(parser.subtree_parser())?);
+                },
+                NodeType::Documents => {
+                    documents = Some(Documents::load(parser.subtree_parser())?);
+                },
+                NodeType::References => {
+                    references = Some(References::load(parser.subtree_parser())?);
+                },
+                NodeType::Definitions => {
+                    definitions = Some(Definitions::load(parser.subtree_parser())?);
+                },
+                NodeType::Objects => {
+                    objects = Some(Objects::load(parser.subtree_parser())?);
+                },
+                NodeType::Connections => {
+                    connections = Some(Connections::load(parser.subtree_parser())?);
+                },
+                NodeType::Takes => {
+                    takes = Some(Takes::load(parser.subtree_parser())?);
+                },
+            }
+        }
+
         Ok(Fbx7400 {
             version: version,
-            nodes: nodes,
+            fbx_header_extension: fbx_header_extension.ok_or_else(|| Error::MissingNode("FBXHeaderExtension".to_owned()))?,
+            file_id: file_id.ok_or_else(|| Error::MissingNode("FileId".to_owned()))?,
+            creation_time: creation_time.ok_or_else(|| Error::MissingNode("CreationTime".to_owned()))?,
+            creator: creator.ok_or_else(|| Error::MissingNode("Creator".to_owned()))?,
+            global_settings: global_settings.ok_or_else(|| Error::MissingNode("GlobalSettings".to_owned()))?,
+            documents: documents.ok_or_else(|| Error::MissingNode("Documents".to_owned()))?,
+            references: references.ok_or_else(|| Error::MissingNode("References".to_owned()))?,
+            definitions: definitions.ok_or_else(|| Error::MissingNode("Definitions".to_owned()))?,
+            objects: objects.ok_or_else(|| Error::MissingNode("Objects".to_owned()))?,
+            connections: connections.ok_or_else(|| Error::MissingNode("Connections".to_owned()))?,
+            takes: takes,
             footer: footer,
         })
+    }
+}
+
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum NodeType {
+    FbxHeaderExtension,
+    FileId(Vec<u8>),
+    CreationTime(String),
+    Creator(String),
+    GlobalSettings,
+    Documents,
+    References,
+    Definitions,
+    Objects,
+    Connections,
+    Takes,
+}
+
+impl NodeType {
+    /// Creates `NodeType` from the given node name.
+    pub fn load<R: ParserSource>(name: &str, mut attrs: Attributes<R>) -> Result<Self> {
+        use parser::binary::utils::AttributeValues;
+
+        match name {
+            "FBXHeaderExtension" => Ok(NodeType::FbxHeaderExtension),
+            "FileId" => {
+                <Vec<u8>>::from_attributes(&mut attrs)
+                    ?
+                    .ok_or_else(|| Error::InvalidAttribute(name.to_owned()))
+                    .map(NodeType::FileId)
+            },
+            "CreationTime" => {
+                <String>::from_attributes(&mut attrs)
+                    ?
+                    .ok_or_else(|| Error::InvalidAttribute(name.to_owned()))
+                    .map(NodeType::CreationTime)
+            },
+            "Creator" => {
+                <String>::from_attributes(&mut attrs)
+                    ?
+                    .ok_or_else(|| Error::InvalidAttribute(name.to_owned()))
+                    .map(NodeType::Creator)
+            },
+            "GlobalSettings" => Ok(NodeType::GlobalSettings),
+            "Documents" => Ok(NodeType::Documents),
+            "References" => Ok(NodeType::References),
+            "Definitions" => Ok(NodeType::Definitions),
+            "Objects" => Ok(NodeType::Objects),
+            "Connections" => Ok(NodeType::Connections),
+            "Takes" => Ok(NodeType::Takes),
+            _ => Err(Error::UnexpectedNode(name.to_owned())),
+        }
+    }
+}
+
+
+/// `FBXHeaderExtension`.
+#[derive(Debug, Clone, PartialEq)]
+pub struct FbxHeaderExtension {
+    /// Child nodes.
+    pub nodes: Vec<GenericNode>,
+}
+
+impl FbxHeaderExtension {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P) -> Result<Self> {
+        let nodes = GenericNode::load_from_parser(&mut parser)?.0;
+        Ok(FbxHeaderExtension { nodes: nodes })
+    }
+}
+
+
+/// `FileId`.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct FileId(pub Vec<u8>);
+
+impl FileId {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P, attrs: Vec<u8>) -> Result<Self> {
+        parser.skip_current_node()?;
+        Ok(FileId(attrs))
+    }
+}
+
+
+/// `CreationTime`.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct CreationTime(pub String);
+
+impl CreationTime {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P, attrs: String) -> Result<Self> {
+        parser.skip_current_node()?;
+        Ok(CreationTime(attrs))
+    }
+}
+
+
+/// `Creator`.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Creator(pub String);
+
+impl Creator {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P, attrs: String) -> Result<Self> {
+        parser.skip_current_node()?;
+        Ok(Creator(attrs))
+    }
+}
+
+
+/// `GlobalSettings`.
+#[derive(Debug, Clone, PartialEq)]
+pub struct GlobalSettings {
+    /// Child nodes.
+    pub nodes: Vec<GenericNode>,
+}
+
+impl GlobalSettings {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P) -> Result<Self> {
+        let nodes = GenericNode::load_from_parser(&mut parser)?.0;
+        Ok(GlobalSettings { nodes: nodes })
+    }
+}
+
+
+/// `Documents`.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Documents {
+    /// Child nodes.
+    pub nodes: Vec<GenericNode>,
+}
+
+impl Documents {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P) -> Result<Self> {
+        let nodes = GenericNode::load_from_parser(&mut parser)?.0;
+        Ok(Documents { nodes: nodes })
+    }
+}
+
+
+/// `References`.
+#[derive(Debug, Clone, PartialEq)]
+pub struct References {
+    /// Child nodes.
+    pub nodes: Vec<GenericNode>,
+}
+
+impl References {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P) -> Result<Self> {
+        let nodes = GenericNode::load_from_parser(&mut parser)?.0;
+        Ok(References { nodes: nodes })
+    }
+}
+
+
+/// `Definitions`.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Definitions {
+    /// Child nodes.
+    pub nodes: Vec<GenericNode>,
+}
+
+impl Definitions {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P) -> Result<Self> {
+        let nodes = GenericNode::load_from_parser(&mut parser)?.0;
+        Ok(Definitions { nodes: nodes })
+    }
+}
+
+
+/// `Objects`.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Objects {
+    /// Child nodes.
+    pub nodes: Vec<GenericNode>,
+}
+
+impl Objects {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P) -> Result<Self> {
+        let nodes = GenericNode::load_from_parser(&mut parser)?.0;
+        Ok(Objects { nodes: nodes })
+    }
+}
+
+
+/// `Connections`.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Connections {
+    /// Child nodes.
+    pub nodes: Vec<GenericNode>,
+}
+
+impl Connections {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P) -> Result<Self> {
+        let nodes = GenericNode::load_from_parser(&mut parser)?.0;
+        Ok(Connections { nodes: nodes })
+    }
+}
+
+
+/// `Takes`.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Takes {
+    /// Child nodes.
+    pub nodes: Vec<GenericNode>,
+}
+
+impl Takes {
+    /// Loads node contents from the parser.
+    pub fn load<R: ParserSource, P: Parser<R>>(mut parser: P) -> Result<Self> {
+        let nodes = GenericNode::load_from_parser(&mut parser)?.0;
+        Ok(Takes { nodes: nodes })
     }
 }
