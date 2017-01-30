dfa44ca611bc3e0bd2308d6b7e502f01d8d4e0f3
Takuma YOSHIOKA <tashioka.256@gmail.com>
2017-01-30 15:48:53 +0900

    Add `loader::binary::simple::Error::Inconsistent` variant

--- src/loader/binary/simple/error.rs
+++ src/loader/binary/simple/error.rs
@@ -13,6 +13,10 @@ pub type Result<T> = ::std::result::Result<T, Error>;
 /// Load error.
 #[derive(Debug, Clone)]
 pub enum Error {
+    /// Inconsistent data.
+    ///
+    /// This means there's schema error among multiple nodes or attributes.
+    Inconsistent(String),
     /// Attribute is invalid.
     ///
     /// This includes the cases below:
@@ -31,6 +35,7 @@ pub enum Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
+            Error::Inconsistent(ref msg) => write!(f, "Inconsistent structure or data: {}", msg),
             Error::InvalidAttribute(ref name) => write!(f, "Invalid attribute for node: {}", name),
             Error::MissingNode(ref name) => write!(f, "Missing node: {}", name),
             Error::UnexpectedNode(ref name) => write!(f, "Unexpected node: {}", name),
@@ -42,6 +47,7 @@ impl fmt::Display for Error {
 impl error::Error for Error {
     fn description(&self) -> &str {
         match *self {
+            Error::Inconsistent(_) => "Inconsistent structure or data",
             Error::InvalidAttribute(_) => "Invalid node attribute",
             Error::MissingNode(_) => "Missing node",
             Error::UnexpectedNode(_) => "Unexpected node",
